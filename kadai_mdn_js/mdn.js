8602de9ac84ba05331e47e2f58b3c322ad252ca4
suga310 <hrigs.0310@icloud.com>
2025-04-28 00:51:19 +0900

    アップロード

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const date=new Date();
+
+const year=date.getFullYear();
+const month=date.getMonth();
+const day=date.getDate();
+
+console.log(date);
+console.log(`${year}年${month + 1}月${day}日`);
+
