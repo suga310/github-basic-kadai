c5471c90be47e345355e77f593dc44ad07ab3691
suga310 <hrigs.0310@icloud.com>
2025-05-18 20:56:56 +0900

    kadai_007アップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>8章課題</title>
+</head>
+
+<body>
+   <p>
+       <?php
+       $array=['name'=>'onion','price'=>200,'weight'=>160];
+       print_r($array);
+
+       ?>
+   </p>
+</body>
+
+</html>
